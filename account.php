357432df32746e4cde0078e1ce11643734809b38
Rodel Makilan <c20234112.makilan@csav.edu.ph>
2025-04-17 23:51:51 +0800

    Add files via upload

---
+++ account.php
@@ -0,0 +1,8 @@
+<?php include 'includes/header.html'; ?>
+<?php include 'includes/navbar.html'; ?>
+
+
+
+
+
+<?php include 'includes/footer.html'; ?>
---
+++ classroom.php
@@ -0,0 +1,13 @@
+<?php include 'includes/header.html' ?>
+<?php include 'includes/navbar.html'?>
+
+<button class="course">
+<div class="card" style="width: 18rem;">
+  <img src="https://avatar.iran.liara.run/public" class="card-img-top" alt="course">
+  <div class="card-body">
+    <p class="card-text d-flex justify-content-center">CCIM-221</p>
+  </div>
+</div>
+</button>
+
+<?php include 'includes/footer.html' ?>
